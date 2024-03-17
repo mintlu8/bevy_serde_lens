80a15ebfbe6b24496f1013caf684716feea147c8
mintlu8 <mintlux667@gmail.com>
2024-03-18 03:31:06 +0800

    Added `bevy_defer` integration.

--- main/src/typetagged.rs
+++ main/src/typetagged.rs
@@ -60,8 +60,16 @@ scoped_thread_local! {
     static DESERIALIZE_FUNCTIONS: TypeTagServer
 }
 
+scoped_thread_local!(
+    static DESERIALIZE_ANY_FUNCTIONS: DeserializeAnyServer
+);
+
 pub(crate) fn scoped<T>(deserialize_fns: &TypeTagServer, f: impl FnOnce() -> T) -> T{
     DESERIALIZE_FUNCTIONS.set(deserialize_fns, f)
+}
+
+pub(crate) fn scoped_any<T>(deserialize_fns: &DeserializeAnyServer, f: impl FnOnce() -> T) -> T{
+    DESERIALIZE_ANY_FUNCTIONS.set(deserialize_fns, f)
 }
 
 /// A serializable trait object.
@@ -72,6 +80,47 @@ pub struct TypeTagged<T: BevyTypeTagged>(T);
 impl<T: BevyTypeTagged> Convert<T> for TypeTagged<T> {
     fn ser(input: &T) -> &Self {
         TypeTagged::ref_cast(input)
+    }
+
+    fn de(self) -> T {
+        self.0
+    }
+}
+
+/// A serializable trait object that uses `deserialize_any`.
+/// 
+/// Serialize has the same behavior as [`TypeTagged`].
+/// 
+/// # Why
+/// 
+/// Normally [`TypeTagged`] deserializes from something like
+/// 
+/// ```
+/// {
+///     "my_field": {
+///         "TypeName": 1.23
+///     }
+/// }
+/// ```
+/// 
+/// This might be cumbersome for human written data so we allow parsing non-maps directly
+/// using `deserialize_any`
+/// 
+/// ```
+/// {
+///     "my_field": 1.23
+/// }
+/// ```
+/// 
+/// Due to the serde specification this is not allowed on non-self-describing formats
+/// like `postcard` and will cause an error, be careful when using this in multiple formats.
+#[derive(Debug, RefCast)]
+#[repr(transparent)]
+pub struct AnyTagged<T: BevyTypeTagged>(T);
+
+impl<T: BevyTypeTagged> Convert<T> for AnyTagged<T> {
+    fn ser(input: &T) -> &Self {
+        AnyTagged::ref_cast(input)
     }
 
     fn de(self) -> T {
@@ -187,7 +236,96 @@ impl TypeTagServer {
     }
 }
 
+type DeserializeUnitFn<T> = fn() -> Result<T, String>;
+type DeserializeBoolFn<T> = fn(bool) -> Result<T, String>;
+type DeserializeIntFn<T> = fn(i64) -> Result<T, String>;
+type DeserializeUIntFn<T> = fn(u64) -> Result<T, String>;
+type DeserializeFloatFn<T> = fn(f64) -> Result<T, String>;
+type DeserializeCharFn<T> = fn(char) -> Result<T, String>;
+type DeserializeStrFn<T> = fn(&str) -> Result<T, String>;
+type DeserializeBytesFn<T> = fn(&[u8]) -> Result<T, String>;
+
+// Experimental
+// type DeserializeSeqFn<T> = fn(&mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>;
+
+/// A [`Resource`] that stores registered deserialize functions from primitives.
+#[derive(Resource, Default)]
+pub struct DeserializeAnyServer {
+    deserialize_unit: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_bool: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_int: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_uint: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_float: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_char: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_str: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_bytes: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+    deserialize_seq: FxHashMap<TypeId, Box<dyn Any + Send + Sync>>,
+}
+
+impl DeserializeAnyServer {
+    pub fn clear(&mut self) {
+        self.deserialize_unit.clear();
+        self.deserialize_bool.clear();
+        self.deserialize_int.clear();
+        self.deserialize_uint.clear();
+        self.deserialize_float.clear();
+        self.deserialize_char.clear();
+        self.deserialize_str.clear();
+        self.deserialize_bytes.clear();
+        self.deserialize_seq.clear();
+    }
+
+    pub fn register_unit<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeUnitFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_unit.insert(id, Box::new(f));
+    }
+
+    pub fn register_bool<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeBoolFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_bool.insert(id, Box::new(f));
+    }
+
+    pub fn register_int<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeIntFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_int.insert(id, Box::new(f));
+    }
+
+    pub fn register_uint<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeUIntFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_uint.insert(id, Box::new(f));
+    }
+
+    pub fn register_float<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeFloatFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_float.insert(id, Box::new(f));
+    }
+
+    pub fn register_char<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeCharFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_char.insert(id, Box::new(f));
+    }
+
+    pub fn register_str<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeStrFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_str.insert(id, Box::new(f));
+    }
+
+    pub fn register_bytes<T: BevyTypeTagged, A: Send + Sync + 'static>(&mut self, f: DeserializeBytesFn<A>) {
+        let id = TypeId::of::<T>();
+        self.deserialize_bytes.insert(id, Box::new(f));
+    }
+}
+
 impl<V> serde::Serialize for TypeTagged<V> where V: BevyTypeTagged {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
+        use serde::ser::SerializeMap;
+        let mut map = serializer.serialize_map(Some(1))?;
+        map.serialize_entry(self.0.name().as_ref(), &self.0.as_serialize())?;
+        map.end()
+    }
+}
+
+impl<V> serde::Serialize for AnyTagged<V> where V: BevyTypeTagged {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
         use serde::ser::SerializeMap;
         let mut map = serializer.serialize_map(Some(1))?;
@@ -198,14 +336,20 @@ impl<V> serde::Serialize for TypeTagged<V> where V: BevyTypeTagged {
 
 impl<'de, V: BevyTypeTagged> serde::Deserialize<'de> for TypeTagged<V> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
-        deserializer.deserialize_map(TypeTaggedVisitor::<V>(PhantomData))
+        deserializer.deserialize_map(TypeTaggedVisitor::<V>(PhantomData)).map(TypeTagged)
+    }
+}
+
+impl<'de, V: BevyTypeTagged> serde::Deserialize<'de> for AnyTagged<V> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
+        deserializer.deserialize_any(TypeTaggedVisitor::<V>(PhantomData)).map(AnyTagged)
     }
 }
 
 struct TypeTaggedVisitor<'de, V: BevyTypeTagged>(PhantomData<&'de V>);
 
 impl<'de, V: BevyTypeTagged> Visitor<'de> for TypeTaggedVisitor<'de, V>  {
-    type Value = TypeTagged<V>;
+    type Value = V;
 
     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(formatter, "externally tagged enum")
@@ -229,7 +373,7 @@ impl<'de, V: BevyTypeTagged> Visitor<'de> for TypeTaggedVisitor<'de, V>  {
                 "unregistered type-tag {}", key,
             )));
         };
-        map.next_value_seed(DeserializeFnSeed(de_fn, PhantomData)).map(TypeTagged)
+        map.next_value_seed(DeserializeFnSeed(de_fn, PhantomData))
     }
 }
 
